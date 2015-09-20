a78e070067216fcae5d6d8087d7685e34583156f
Sheryl Boughton <sherpbo@gmail.com>
2015-09-20 08:46:36 -0700

    hide modal on login

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -3,10 +3,10 @@ angular.module('clubApp')
   $scope.test = "hi";
 
   $scope.login = function(u, p){
-    $scope.hideLogin = true;
     var loginInfo = {username: u, password: p};
     $http.post("/login", loginInfo)
     .then(function(data){
+      $scope.hideLogin = true;
       console.log(data);
       $rootScope.currentUser = data;
       $state.go("where-to");
