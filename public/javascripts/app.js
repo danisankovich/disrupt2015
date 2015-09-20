533cce92be241191d2a696427d0be66c1d0368b6
Michael Sankovich <misankovich@gmail.com>
2015-09-19 22:56:33 -0700

    profile

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,7 +4,6 @@ var app = angular.module('clubApp', ['ui.router']);
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
   $stateProvider
-<<<<<<< HEAD
   .state('map', {
     url: '/map',
     templateUrl: 'views/map.ejs',
@@ -128,41 +127,3 @@ app.controller('mapCtrl', function($scope) {
     // additional code to create a map and directions widget
   });
 });
-=======
-    .state('map', {
-      url: '/map',
-      templateUrl: 'views/map.ejs',
-    })
-    .state('splash', {
-      url: '/splash',
-      templateUrl: 'views/splash.ejs',
-      controller: 'splashCtrl'
-    })
-    .state('where-to', {
-      url: '/where-to',
-      templateUrl: 'views/where-to.ejs',
-      controller: 'clubCtrl'
-    })
-    .state('home', {
-      url: '/',
-      templateUrl: 'views/home.ejs',
-    })
-    .state('search', {
-      url: '/search',
-      templateUrl: 'views/search.ejs',
-      controller: 'searchCtrl'
-    })
-    .state('mycollection', {
-      url: '/mycollection',
-      templateUrl: 'views/mycollection.ejs',
-    })
-    .state('myclubs', {
-      url: '/myclubs',
-      templateUrl: 'views/myclubs.ejs',
-    })
-    .state('about', {
-      url: '/about',
-      templateUrl: 'views/about.ejs',
-    });
-}]);
->>>>>>> b4ceeb4aa3ed98bdc2da12c60dfc013fac1c5e23
