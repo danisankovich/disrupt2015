c6cd22eb93adfdcb0816522c5dcb8af288169473
Sheryl Boughton <sherpbo@gmail.com>
2015-09-19 22:58:12 -0700

    set up to receive preferences

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var request = require("request");
-var http = require("http");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -14,7 +13,7 @@ router.post('/login', function(req, res, next) {
   var username = req.body.username;
   var password = req.body.password;
   console.log(username, password);
-  var options = {
+  request.get({
     'auth': {
       'user': username,
       'pass': password,
@@ -22,8 +21,7 @@ router.post('/login', function(req, res, next) {
     },
     url: "https://api.dz.zalan.do/auth/token",
     headers: {"Accept": "application/x.zalando.myfeed+json;version=2"}
-  };
-  function callback(error, response, body) {
+  }, function(error, response, body) {
     if(error){
       console.error(error);
       res.send(400);
@@ -35,16 +33,14 @@ router.post('/login', function(req, res, next) {
     var access_type = body.access_type;
     console.log(access_token, access_token);
     authData = access_type + " " + access_token;
-    // res.redirect(200, "/profile");
-    http.get("http://localhost:8000/profile", function(err, res, body){
+    request.get("http://localhost:8000/profile", function(err, res, body){
       if(err){
         console.log(err);
       }
-      console.log(body);
+      console.log("~~~~~~IN LOGIN~~~~~~", body);
     });
     res.send(200);
-  }
-  request.get(options, callback);
+  });
 });
 
 router.get('/profile', function(req, res, next) {
@@ -57,6 +53,13 @@ router.get('/profile', function(req, res, next) {
       console.error(error);
     }
     console.log(body);
+    // curl -X GET --header "Accept: application/x.zalando.myfeed+json;version=2" --header "Authorization: User 83cdd68a-24e9-4efa-928b-99a7b538be64" "https://api.dz.zalan.do/customer-profiles/83001065008/preferences"
+    request.get("https://api.dz.zalan.do/customer-profiles/"+ body.profile_id +"/preferences", function(err, res, preferences){
+      if(err){
+        console.error(err);
+      }
+      console.log("here are your preferences:", preferences);
+    });
   });
 });
 // curl -X GET --header "Accept: application/x.zalando.myfeed+json;version=2" --header "Authorization: User 846yeerf-d4cq-4903-9hc8-f0f034fed1bb" "https://api.dz.zalan.do/customer-profiles/1226542314"
