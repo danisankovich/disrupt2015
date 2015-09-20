3a2969f9281a0943c639755d413cd3a825ab40e9
Michael Sankovich <misankovich@gmail.com>
2015-09-20 05:57:32 -0700

    getting there

--- public/javascripts/clothingsearch.js
+++ public/javascripts/clothingsearch.js
@@ -2,51 +2,59 @@ angular.module('clubApp')
 .controller('searchCtrl', function($scope, $state) {
   $scope.items = [
     {
-      name: 'leggings',
-      style: 'awesome',
-      brands: 'expensive ones',
+      img: "https://secure-i4.ztat.net//detail/LV/02/1B/00/1K/11/LV021B001-K11@12.jpg",
+      name: 'A-line skirt - navy',
+      style: 'Knee-length',
+      brands: 'Love',
       compitem: 'those shirts'
     },
     {
-      name: 'leggings',
-      style: 'awesome',
-      brands: 'expensive ones',
+      img: "https://secure-i6.ztat.net//detail/OA/12/1L/01/GQ/11/OA121L01G-Q11@14.jpg",
+      name: 'Leather jacket - black',
+      style: 'leather',
+      brands: 'Oakwood',
       compitem: 'those shirts'
     },
     {
-      name: 'leggings',
-      style: 'awesome',
-      brands: 'expensive ones',
+      img: "https://secure-i3.ztat.net//detail/C5/15/1J/01/H1/03/C5151J01H-103@6.jpg",
+      name: 'Tights - anthracite',
+      style: 'tights',
+      brands: 'Camano',
       compitem: 'those shirts'
     },
     {
-      name: 'leggings',
-      style: 'awesome',
-      brands: 'expensive ones',
+      img: 'https://secure-i2.ztat.net//detail/AN/62/1C/A2/7I/11/AN621CA27-I11@14.jpg',
+      name: 'Jersey dress - purple',
+      style: 'Cache-coeur',
+      brands: 'Anna Field',
       compitem: 'those shirts'
     },
     {
-      name: 'leggings',
-      style: 'awesome',
-      brands: 'expensive ones',
+      img: 'https://secure-i5.ztat.net//detail/TS/12/1B/00/4Q/11/TS121B004-Q11@8.jpg',
+      name: 'Mini skirt - noir',
+      style: 'mini-skirt',
+      brands: 'Teddy Smith',
       compitem: 'those shirts'
     },
     {
-      name: 'leggings',
-      style: 'awesome',
-      brands: 'expensive ones',
+      img: 'https://secure-i6.ztat.net//detail/MA/32/2D/07/BK/11/MA322D07B-K11@18.jpg',
+      name: 'Shirt - blue',
+      style: 'Shaped-fit',
+      brands: 'Marc O\'Polo',
       compitem: 'those shirts'
     },
     {
-      name: 'leggings',
-      style: 'awesome',
-      brands: 'expensive ones',
+      img: 'https://secure-i4.ztat.net//detail/ES/12/2E/01/4C/11/ES122E014-C11@14.jpg',
+      name: 'Chinos - grey',
+      style: 'Flow',
+      brands: 'Espirit',
       compitem: 'those shirts'
     },
     {
-      name: 'leggings',
-      style: 'awesome',
-      brands: 'expensive ones',
+      img: "https://secure-i3.ztat.net//detail/WR/12/2G/02/0C/11/WR122G020-C11@11.jpg",
+      name: 'Straight leg jeans',
+      style: 'stonewash',
+      brands: 'Wrangler',
       compitem: 'those shirts'
     }
   ];
