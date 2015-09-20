aca420b3a94689670de76f1db7eebeef3fe41944
Michael Sankovich <misankovich@gmail.com>
2015-09-19 19:16:36 -0700

    Update app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -24,34 +24,108 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
       url: '/where-to',
       templateUrl: 'views/where-to.html',
       controller: 'clubCtrl'
-    });
-    // .state('login', {
-    //   url: '/login',
-    //   templateUrl: 'views/login.ejs',
-    //   controller: 'dashboardCtrl'
-    // })
-    // .state('about', {
-    //   url: '/about',
-    //   templateUrl: 'views/about.ejs',
-    //   controller: 'aboutCtrl'
-    // })
-    // .state('account', {
-    //   url: '/account',
-    //   templateUrl: 'views/account.ejs',
-    //   controller: 'dashboardCtrl'
-    // })
-    // .state('dashboard', {
-    //   url: '/dashboard',
-    //   templateUrl: 'views/dashboard.ejs',
-    //   controller: 'dashboardCtrl'
-    // })
-    // .state('search', {
-    //   url: '/search',
-    //   templateUrl: 'views/search.ejs',
-    //   controller: 'searchCtrl'
-    // })
-    // .state('signup', {
-    //   url: '/signup',
-    //   templateUrl: 'views/register.ejs',
-    // });
+    })
+    .state('home', {
+      url: '/',
+      templateUrl: 'views/home.ejs',
+    })
+    .state('search', {
+      url: '/search',
+      templateUrl: 'views/search.ejs',
+      controller: 'searchCtrl'
+    })
+    .state('profile', {
+      url: '/profile',
+      templateUrl: 'views/profile.ejs',
+      // controller: 'aboutCtrl'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: 'views/about.ejs',
+    })
 }]);
+
+app.controller('searchCtrl', function($scope, $state) {
+  $scope.items = [
+    {
+      name: 'leggings',
+      style: 'awesome',
+      brands: 'expensive ones',
+      compitem: 'those shirts'
+    },
+    {
+      name: 'leggings',
+      style: 'awesome',
+      brands: 'expensive ones',
+      compitem: 'those shirts'
+    },
+    {
+      name: 'leggings',
+      style: 'awesome',
+      brands: 'expensive ones',
+      compitem: 'those shirts'
+    },
+    {
+      name: 'leggings',
+      style: 'awesome',
+      brands: 'expensive ones',
+      compitem: 'those shirts'
+    },
+    {
+      name: 'leggings',
+      style: 'awesome',
+      brands: 'expensive ones',
+      compitem: 'those shirts'
+    },
+    {
+      name: 'leggings',
+      style: 'awesome',
+      brands: 'expensive ones',
+      compitem: 'those shirts'
+    },
+    {
+      name: 'leggings',
+      style: 'awesome',
+      brands: 'expensive ones',
+      compitem: 'those shirts'
+    },
+    {
+      name: 'leggings',
+      style: 'awesome',
+      brands: 'expensive ones',
+      compitem: 'those shirts'
+    }
+  ];
+  console.log($scope.items);
+});
+
+
+app.controller('mapCtrl', function($scope) {
+
+var map;
+
+require(["esri/map", "dojo/domReady!"], function(Map) {
+  map = new Map("map", {
+    basemap: "topo",  //For full list of pre-defined basemaps, navigate to http://arcg.is/1JVo6Wd
+    center: [-122.45, 37.75], // longitude, latitude
+    zoom: 13
+  });
+});
+
+require([
+  "esri/map",
+  "esri/dijit/Directions",
+  "dojo/parser",
+  "dijit/layout/BorderContainer", "dijit/layout/ContentPane",
+  "dojo/domReady!"
+], function (Map, Directions, parser) {
+    // call the parser
+    parser.parse();
+    var directions = new Directions({
+  map: map
+}, "dir");
+directions.startup();
+    // additional code to create a map and directions widget
+});
+});
+
